56a44192b98d2f233019803b9c6336b6b46d44f1
Ganeshan Sudhakar <ganeshansudhakar01@gmail.com>
2024-11-23 14:18:38 +0530

    Add Space

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, FastAPI on AWS!"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
